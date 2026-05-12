069436d63bd3f963361f607a54adcff856ef3a84
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2026-05-12 19:06:53 +0300

    new settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, StudentProfile, AdvisorProfile
+
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(StudentProfile)
+admin.site.register(AdvisorProfile)
---
+++ appointments/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from accounts.models import StudentProfile, AdvisorProfile
+
+# Create your models here.
+class Appointment(models.Model):
+
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("approved", "Approved"),
+        ("done", "Done"),
+        ("canceled", "Canceled"),
+    ]
+
+    student = models.ForeignKey(
+        StudentProfile,
+        on_delete=models.CASCADE
+    )
+
+    advisor = models.ForeignKey(
+        AdvisorProfile,
+        on_delete=models.CASCADE
+    )
+
+    date = models.DateTimeField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="pending"
+    )
+
+    def __str__(self):
+        return f"{self.student.user.username} with {self.advisor.user.username}"
