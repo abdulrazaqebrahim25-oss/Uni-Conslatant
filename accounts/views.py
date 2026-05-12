33da03e4b6ff8e50e54d493ebf12f49fb98a5522
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2026-05-12 19:46:14 +0300

    form of reg

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
+
+from django.shortcuts import render, redirect
+
+from .forms import RegisterForm
+from .models import StudentProfile, AdvisorProfile
+
 # Create your views here.
+def register_view(request):
+
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            user = form.save()
+
+            if user.role == "student":
+
+                StudentProfile.objects.create(
+                    user=user
+                )
+
+            elif user.role == "advisor":
+
+                AdvisorProfile.objects.create(
+                    user=user
+                )
+
+            return redirect("login")
+
+    else:
+
+        form = RegisterForm()
+
+    return render(
+        request,
+        "registration/register.html",
+        {
+            "form": form
+        }
+    )
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 
 
 urlpatterns = [
-   
+       path(
+        "accounts/",
+        include("accounts.urls")
+    ),
+
 ]
