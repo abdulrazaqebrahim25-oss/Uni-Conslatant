a829b9cbd72f5d22cbfc43ee6efe033b00427003
abdulrazaqebrahim25-oss <abdulrazaqebrahim25@gmail.com>
2026-05-15 19:38:44 +0300

    Add new prefreeance to form and start with appointment

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import create_appointment
+from . import views 
 
 urlpatterns = [
     path('create/', create_appointment, name='create_appointment'),
+        path(
+        'student/',
+        views.student_appointments,
+        name='student_appointments'
+    ),
 ]
